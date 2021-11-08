10488a2c7f63f83623b02b6a97c332a55d99bde6
Alberto <71991222+CarlosTijeritas@users.noreply.github.com>
2021-11-07 23:37:05 -0600

    Update ConstruccionAFN.cpp

--- Practica2-ALG_THOMPSON/ConstruccionAFN.cpp
+++ Practica2-ALG_THOMPSON/ConstruccionAFN.cpp
@@ -24,4 +24,47 @@ AFN unir(AFN pReferencia,AFN sReferencia,int n){
         pReferencia.estadoFinal[0]=pReferencia.nodos[numNodos-1];
         return pReferencia;
 }
+
+
+
+
+AFN construyeAFN(vector<char> expresionPostfija){
+    int numNodos=0;
+
+    vector<AFN> refAutomata;
+    
+    for(auto caracter:expresionPostfija){
+       
+       if( caracter == '.'){
+             int tamanio=refAutomata.size();
+            
+             AFN segundaReferencia=refAutomata[tamanio-1];
+             AFN primeraReferencia=refAutomata[tamanio-2];
+             
+             refAutomata.pop_back();
+             refAutomata.pop_back();
+             refAutomata.push_back(concatenar(primeraReferencia,segundaReferencia,numNodos));
+       }
+       else if(caracter=='|'){
+            
+            int tamanio=refAutomata.size();
+            AFN segundaReferencia=refAutomata[tamanio-1];
+            AFN primeraReferencia=refAutomata[tamanio-2];
+            refAutomata.pop_back();
+            refAutomata.pop_back();
+            refAutomata.push_back(unir(primeraReferencia,segundaReferencia,numNodos));
+            numNodos+=2;
+       }
+       else if(caracter=='*'){
+            int tamanio=refAutomata.size();
+           
+            AFN primeraReferencia=refAutomata[tamanio-1];
+            refAutomata.pop_back();
+            refAutomata.push_back(cerradura(primeraReferencia,numNodos));
+            numNodos+=2;
+       }else{
+            refAutomata.push_back(AFN(caracter,numNodos));
+            numNodos+=2;
+       }  
+    }
   
