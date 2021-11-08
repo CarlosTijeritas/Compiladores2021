2ad5063668b164679ee14e5d188996ad6e84afaa
Alberto <71991222+CarlosTijeritas@users.noreply.github.com>
2021-11-07 22:26:03 -0600

    Update ConstruccionAFN.cpp

--- Practica2-ALG_THOMPSON/ConstruccionAFN.cpp
+++ Practica2-ALG_THOMPSON/ConstruccionAFN.cpp
@@ -10,4 +10,18 @@ AFN unir(AFN pReferencia,AFN sReferencia,int n){
         pReferencia.alfabeto.insert(pReferencia.alfabeto.end(),sReferencia.alfabeto.begin(),sReferencia.alfabeto.end()); 
         pReferencia.nodos.push_back(n);
         pReferencia.nodos.push_back(n+1);
+        pReferencia.transiciones.merge(sReferencia.transiciones);
+        map<int, string> transiciones_epsilon;
+        int numNodos=pReferencia.nodos.size();
+        string transicion_1="E : [" +to_string(pReferencia.estadoInicial)+" , "+to_string(sReferencia.estadoInicial)+"]";
+        transiciones_epsilon.insert(make_pair(pReferencia.nodos[numNodos-2],transicion_1));
+        string transicion_2="E : [" +to_string(pReferencia.nodos[numNodos-1])+"]";
+        transiciones_epsilon.insert(make_pair(pReferencia.estadoFinal[0],transicion_2));
+        string transicion_3="E : [" +to_string(pReferencia.nodos[numNodos-1])+"]";
+        transiciones_epsilon.insert(make_pair(sReferencia.estadoFinal[0],transicion_3));
+        pReferencia.transiciones.merge(transiciones_epsilon);
+        pReferencia.estadoInicial=pReferencia.nodos[numNodos-2];
+        pReferencia.estadoFinal[0]=pReferencia.nodos[numNodos-1];
+        return pReferencia;
+}
   
